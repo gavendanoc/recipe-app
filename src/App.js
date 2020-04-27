969905ae935af97697f527f455178003cc7fe63f
gabriel <gabriel19oct@hotmail.com>
2020-04-26 21:52:09 -0500

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import {APP_KEY, APP_ID} from './keys/edamamKeys';
 import './App.css';
 
 function App() {
+  const exmapleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Recat</h1>
     </div>
   );
 }
