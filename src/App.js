281c2c09fe85592601b2ca641fe4b3510add7760
gabriel <gabriel19oct@hotmail.com>
2020-04-26 22:29:26 -0500

    tested useEffect and its effects... got it?, I guess you didn't understand the joke :v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {APP_KEY, APP_ID} from './keys/edamamKeys';
 import './App.css';
 
+
+/** 
+
+Runs every time a page re-renders
+useEffect(()=> {
+  console.log('Effect has been run')
+});
+
+Runs only one time, check second argument
+useEffect(() => {
+  console.log('Effect has run');
+}, []);
+
+
+Runs when state 'counter' changes
+useEffect(()=>{
+  console.log('Effect has run');
+}, [counter]);
+*/
+
 function App() {
-  const exmapleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  // runs every time the page re-renders
+  useEffect(()=> {
+    console.log('Effect has been run')
+  }, []);
+
+  const getREcipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>Hello Recat</h1>
+      <form className="Search-form">
+        <input className="search-bar" type="text"></input>
+        <button 
+          className="search-button" 
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
